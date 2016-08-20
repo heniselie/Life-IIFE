9644a2ff7e6dba62b7f11ce14a6c7537ee3389a4
henok tessema <heniselie@yahoo.com>
2016-08-20 10:11:19 -0500

    main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+//console.log("main.js");
+
+
+
+ console.log("Sedan IIFE", Sedan);
+ console.log("Sedan Color", Sedan.getColor());
+  console.log("Sedan Color", Sedan.getType());
+
+
+  console.log("Sedan set Occupancy", Sedan.setOccupancy(6));
+  console.log("Sedan get Occupancy", Sedan.getOccupancy());
